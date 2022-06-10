ca72c4da39ee388e2260edb26266893832fe4506
Rakesh Soni <rakesh.soni03@yahoo.in>
2022-06-11 00:30:50 +0530

    add aws exports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import React, { useEffect, useState } from "react";
+import { withAuthenticator, Button, Heading } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 
 /* src/App.js */
 function App({ signOut, user }) {
-  // Todo logic here
+  const [email, setEmail] = useState();
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().then((user) => {
+      setEmail(user.attributes.email);
+    });
+  }, []);
 
   return (
     <>
       {/* Add Todo JSX here  */}
-      <Heading level={1}>Hello {user.username}</Heading>
+      <Heading level={1}>
+        Hello, {user.username}, email: {email}
+      </Heading>
       <Button onClick={signOut}>Sign out</Button>
     </>
   );
